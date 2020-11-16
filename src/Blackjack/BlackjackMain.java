50dda71c4a85eae8d3f2a8cfaebbcaf6a69288e6
vv1020 <vv1020@wildcats.unh.edu>
2020-11-16 15:54:08 -0500

    Created the basic structure of the project

---
+++ src/Blackjack/BlackjackMain.java
@@ -0,0 +1,18 @@
+package Blackjack;
+
+public class BlackjackMain {
+	Deck playerdeck =new Deck();
+	Deck Dealercard = new Deck();
+	double playeramount;
+	
+	public void dealer() {
+		
+	}
+	public void player() {
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/Blackjack/Card.java
@@ -0,0 +1,13 @@
+package Blackjack;
+
+public class Card{
+	private Suit suit;
+	private Value value;
+	
+	public Card(Suit suit, Value value){}
+	public String getSuit(){
+	}
+	public Value getValue() {
+	}
+	
+}
